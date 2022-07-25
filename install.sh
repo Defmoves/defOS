53cd0772d3c305362b65f8df7e96e4cf66393bd6
defmoves <defmoves@pm.me>
2022-07-25 21:22:13 +0100

    started debian based install

---
+++ install.sh
@@ -0,0 +1,26 @@
+# Fish
+sudo apt-add-repository ppa:fish-shell/release-3
+
+# Codium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+
+sudo apt update
+sudo apt install -y fish Codium
+
+# Starship
+curl -sS https://starship.rs/install.sh | sh
+
+# Installing fonts
+dnf install fontawesome-fonts fontawesome-fonts-web
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d /usr/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d /usr/share/fonts
+# Reloading Font
+fc-cache -vf
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
